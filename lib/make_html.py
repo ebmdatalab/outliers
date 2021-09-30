179581428ed3d84846415b599853d6bb3aff99e5
Jon Massey <jon.massey@thedatalab.org>
2021-09-30 10:08:58 +0000

    feat: nicer entity name printing

--- lib/make_html.py
+++ lib/make_html.py
@@ -9,13 +9,16 @@ import jinja2
 def df_to_html(df):
     return markupsafe.Markup(df.to_html(escape=True, classes=["table","thead-light","table-bordered","table-sm"])).unescape()
 
+def selective_title(str):
+    ALLCAPS = ['NHS','PCN','CCG']
+    return ' '.join([w.title() if w not in ALLCAPS else w for w in str.split(' ')])
 
 def write_to_template(entity_name, table_high, table_low, output_file):
     with open("../data/template.html") as f:
         template = jinja2.Template(f.read())
 
     context = {
-        "entity_name": entity_name,
+        "entity_name": selective_title(entity_name),
         "table_high": df_to_html(table_high),
         "table_low": df_to_html(table_low),
     }
