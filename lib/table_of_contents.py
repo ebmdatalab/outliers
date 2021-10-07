087c38180cadec605d085b9c12d671877df7eb16
Jon Massey <jon.massey@thedatalab.org>
2021-10-07 10:21:36 +0100

    feat: add docstrings, minor refactor

--- lib/table_of_contents.py
+++ lib/table_of_contents.py
@@ -2,26 +2,78 @@ import os.path
 
 
 class MarkdownToC:
+    """
+    Builds Markdown table of contents with optional custom heading and URLs
+    """
     md = ""
     files = {}
     urlprefix = ""
 
-    def __init__(self, urlprefix):
-        self.md = "# Table of contents"
-        self.urlprefix = urlprefix
+    def __init__(self, urlprefix=None, heading=None):
+        """
+        MarkdownToC constructor
+
+        Parameters
+        ----------
+        urlprefix : str, optional
+            prefix to be appended to the URL of each item
+            added to the table of contents
+        heading : str, optional
+            override standard "Table of Contents" header
+        """
+        self.md = "# Table of contents" if not heading else heading
+        self.urlprefix = urlprefix or ""
 
     def add_file(self, path, entity=""):
+        """
+        Adds a file to the table of contents
+
+        Parameters
+        ----------
+        path : str
+            relative path to the item
+        entity : str, optional
+            entity type of the item
+        """
         if entity not in self.files.keys():
             self.files[entity] = [path]
         else:
             self.files[entity].append(path)
 
     def write_toc(self, output_path):
+        """
+        Write table of contents as Markdown to README.md in output path
+
+        Parameters
+        ----------
+        output_path : str
+            directory to write markdown, items are assumed to be in same
+            or sub-directory for relative path derivation
+        """
         tocfile = output_path + "/README.md"
         with open(tocfile, "w") as f:
             f.write(self.__print_toc(output_path))
 
     def __print_toc(self, output_path):
+        """
+        Renders table of contents as Markdown
+
+        Derives path of each item relative to the output path by finding
+        common parent directories and removing. If your item paths and
+        output paths do not have the same base then this may
+        produce unexpected behaviour.
+
+        Parameters
+        ----------
+        output_path : str
+            directory where markdown file will eventually be written
+            items are assumed to be in same,
+            or sub-directory for relative path derivation
+        Returns
+        -------
+        toc : str
+            table of contents in Markdown format
+        """
         toc = self.md
         for entity in self.files.keys():
             toc = toc + "\n" + f"* {entity}"
@@ -42,10 +94,20 @@ class MarkdownToC:
 
     @staticmethod
     def __split_all(path):
-        """splits a path into a list of all its elements
+        """
+        splits a path into a list of all its elements using the local separator
 
         adapted from
         https://www.oreilly.com/library/view/python-cookbook/0596001673/ch04s16.html
+
+        Parameters
+        ----------
+        path : str
+            path to split, OS agnostic
+        Returns
+        -------
+        allparts : list
+            list of all parts of the path as strings
         """
         allparts = []
         while 1:
