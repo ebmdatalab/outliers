5b133a501af54d0c27f31c5d281128b2efe61801
Jon Massey <jon.massey@thedatalab.org>
2021-10-07 10:37:32 +0100

    chore: flake8 nits

--- lib/outliers.py
+++ lib/outliers.py
@@ -26,14 +26,25 @@ pd.DataFrame.to_html = lambda df, *args, **kwargs: (
 )
 
 
-######## Plots ########
-
-
+# Plots
 def bw_scott(x):
-    """Adapted from https://www.statsmodels.org/stable/_modules/statsmodels/nonparametric/bandwidths.html
+    """
+    Scott's Rule of Thumb for bandwidth estimation
+
+    Adapted from
+    https://www.statsmodels.org/stable/_modules/statsmodels/nonparametric/bandwidths.html
     previouly cause an issue where the IQR was 0 for many
     pandas.DataFrame.plot.kde does an okay job using scipy method,
     but haven't worked out how to do that
+
+    Parameters
+    ----------
+    x : array_like
+        Array for which to get the bandwidth
+    Returns
+    -------
+    bw : float
+        The estimate of the bandwidth
     """
 
     def _select_sigma(X):
@@ -68,10 +79,16 @@ def dist_plot(org_value, distribution, figsize=(3.5, 1), **kwargs):
     fig, ax = plt.subplots(1, 1, figsize=figsize, **kwargs)
     distribution = distribution[~np.isnan(distribution)]
     sns.kdeplot(
-        distribution, bw=bw_scott(distribution), ax=ax, linewidth=0.9, legend=False
+        distribution,
+        bw=bw_scott(distribution),
+        ax=ax,
+        linewidth=0.9,
+        legend=False,
     )
     ax.axvline(org_value, color="r", linewidth=1)
-    lower_limit = max(0, min(np.quantile(distribution, 0.001), org_value * 0.9))
+    lower_limit = max(
+        0, min(np.quantile(distribution, 0.001), org_value * 0.9)
+    )
     upper_limit = max(np.quantile(distribution, 0.999), org_value * 1.1)
     ax.set_xlim(lower_limit, upper_limit)
     ax = remove_clutter(ax)
@@ -142,7 +159,7 @@ def html_plt(plt):
     return html_plot
 
 
-######## Entity & bnf names ########
+# Entity & bnf names
 def get_entity_names(name, measure):
     """Takes entity name from entity_names_query and converts into a
     link to the corresponding measure on OpenPrescribing
@@ -193,7 +210,9 @@ def entity_names_query(entity_type):
     WHERE
       name IS NOT NULL
     """
-    entity_names = bq.cached_read(query, csv_path=f"../data/{entity_type}_names.csv")
+    entity_names = bq.cached_read(
+        query, csv_path=f"../data/{entity_type}_names.csv"
+    )
     return entity_names.set_index("code")
 
 
@@ -245,11 +264,13 @@ def bnf_query(bnf_code, bnf_name):
       {bnf_name} IS NOT NULL
     """
     bnf_names = bq.cached_read(query, csv_path=f"../data/{bnf_name}_names.csv")
-    bnf_names = pd.read_csv(f"../data/{bnf_name}_names.csv", dtype={bnf_code: str})
+    bnf_names = pd.read_csv(
+        f"../data/{bnf_name}_names.csv", dtype={bnf_code: str}
+    )
     return bnf_names.set_index(bnf_code)
 
 
-######## Static outliers ########
+# Static outliers
 def fill_zeroes(df, entity_type, denom_code, num_code):
     """Adds missing rows with 0s to fill where there is prescribing in a
     specific denominator column, but no prescribing in the numerator
@@ -276,7 +297,9 @@ def fill_zeroes(df, entity_type, denom_code, num_code):
     entities = df[[entity_type]].drop_duplicates()
     entities["tmp"] = 1
     all_chem_map = entities.merge(chems, on="tmp").drop("tmp", axis=1)
-    df = df.merge(all_chem_map, on=[entity_type, denom_code, num_code], how="outer")
+    df = df.merge(
+        all_chem_map, on=[entity_type, denom_code, num_code], how="outer"
+    )
     df = df.fillna(0)
     df["numerator"] = df["numerator"].astype(int)
     return df
@@ -301,26 +324,32 @@ def static_data_reshaping(df, entity_type, denom_code, num_code):
     pandas df
         dataframe ready to be passed to get_stats
     """
-    ## Drop unnecessary columns
+    # Drop unnecessary columns
     df = df[[entity_type, num_code, "numerator", denom_code]]
 
-    ## Aggregate by pcn/ccg
+    # Aggregate by pcn/ccg
     if entity_type != "practice":
-        df = df.groupby([entity_type, denom_code, num_code], as_index=False).sum()
+        df = df.groupby(
+            [entity_type, denom_code, num_code], as_index=False
+        ).sum()
 
-    ## Fill zeroes where there is some prescribing within that subpara
+    # Fill zeroes where there is some prescribing within that subpara
     df = fill_zeroes(df, entity_type, denom_code, num_code)
 
-    ## Merge BNF names
+    # Merge BNF names
     for x in [num_code, denom_code]:
-        df = df.merge(get_bnf_names(x), how="left", left_on=x, right_index=True)
+        df = df.merge(
+            get_bnf_names(x), how="left", left_on=x, right_index=True
+        )
 
-    ## Calculate denominator
+    # Calculate denominator
     df = df.set_index([entity_type, denom_code, num_code])
-    df["denominator"] = df.groupby([entity_type, denom_code]).sum()["numerator"]
+    df["denominator"] = df.groupby([entity_type, denom_code]).sum()[
+        "numerator"
+    ]
     df = df.reset_index()
 
-    ## Calculate ratio
+    # Calculate ratio
     df["ratio"] = df["numerator"] / df["denominator"]
 
     return df.set_index([entity_type, num_code])
@@ -361,7 +390,9 @@ def trim_outliers(df, measure, aggregators):
         pandas Series
             Trimmed Series.
         """
-        mask = (series >= series.quantile(0.001)) & (series <= series.quantile(0.999))
+        mask = (series >= series.quantile(0.001)) & (
+            series <= series.quantile(0.999)
+        )
         return mask
 
     mask = df.groupby(aggregators)[measure].apply(lambda x: trim_series(x))
@@ -381,7 +412,8 @@ def get_stats(df, measure, aggregators, stat_parameters, trim=True):
     aggregators : list
         Column(s) to use for aggregating data.
     stat_parameters : list
-        Additional parameters to be calculated, e.g. ["skew", pd.DataFrame.kurt]
+        Additional parameters to be calculated
+        e.g. ["skew", pd.DataFrame.kurt]
     trim : bool
         Say whether to trim the data before calculating stats
 
@@ -404,8 +436,9 @@ def get_stats(df, measure, aggregators, stat_parameters, trim=True):
     df["z_score"] = (df[measure] - stats["mean"]) / stats["std"]
     # df['z_score'] = df['z_score'].abs() # change to absolute values
 
-    ## Some chemicals had a std of ~0 making inf values
-    ## I think this identifies where e.g. CCGs are the only ones to prescribe this.
+    # Some chemicals had a std of ~0 making inf values
+    # I think this identifies where e.g. CCGs
+    # are the only ones to prescribe this.
     df = df.replace([np.inf, -np.inf], np.nan)
     df = df.dropna()
     return df
@@ -428,7 +461,8 @@ class StaticOutlierStats:
     denom_code : str
         Column name for denominator codes
     stat_parameters :
-        Additional parameters to be calculated, e.g. ["skew", pd.DataFrame.kurt]
+        Additional parameters to be calculated
+        e.g. ["skew", pd.DataFrame.kurt]
     trim : bool
         Say whether to trim the data before calculating stats
     """
@@ -547,7 +581,9 @@ def create_out_table(df, attr, entity_type, table_length, ascending):
         Dataframe containing rows for all entities that will be made into HTML
         tables
     """
-    out_table = sort_pick_top(df, "z_score", ascending, entity_type, table_length)
+    out_table = sort_pick_top(
+        df, "z_score", ascending, entity_type, table_length
+    )
     out_table = join_measure_array(df, out_table, attr.measure)
     return out_table
 
@@ -659,8 +695,10 @@ def loop_over_everything(
     template_path : str
         Path to jinja2 html template file
     """
-    urlprefix = "https://htmlpreview.github.io/?" + \
-        "https://raw.githubusercontent.com/ebmdatalab/outliers/master/"
+    urlprefix = (
+        "https://htmlpreview.github.io/?"
+        + "https://raw.githubusercontent.com/ebmdatalab/outliers/master/"
+    )
     toc = MarkdownToC(urlprefix)
 
     for ent_type in entities:
@@ -669,7 +707,7 @@ def loop_over_everything(
             df=df,
             entity_type=ent_type,
             num_code="chemical",
-            denom_code="subpara"
+            denom_code="subpara",
         )
         stats = stats_class.get_table()
 
@@ -691,9 +729,10 @@ def loop_over_everything(
         codes = stats.index.get_level_values(0).unique()[0:10]
         for code in tqdm(codes, desc=f"Writing HTML: {ent_type}"):
             output_file = path.join(
-                    output_dir,
-                    'html',
-                    f"static_{ent_type}_{code}.html",)
+                output_dir,
+                "html",
+                f"static_{ent_type}_{code}.html",
+            )
             write_to_template(
                 entity_names.loc[code, "name"],
                 get_entity_table(table_high, stats_class, code),
@@ -705,7 +744,7 @@ def loop_over_everything(
     toc.write_toc(output_dir)
 
 
-######## Change outliers ########
+# Change outliers
 def sparkline_series(df, column, subset=None):
     """Creates a pandas series containing sparkline plots, based on a
     specific column in a dataframe.
@@ -739,7 +778,9 @@ def sparkline_series(df, column, subset=None):
 
 
 def sparkline_table(change_df, name, measure):
-    data = pd.read_csv("../data/{}/bq_cache.csv".format(name), index_col="code")
+    data = pd.read_csv(
+        "../data/{}/bq_cache.csv".format(name), index_col="code"
+    )
     data["month"] = pd.to_datetime(data["month"])
     data["rate"] = data["numerator"] / data["denominator"]
     data = data.sort_values("month")
@@ -747,18 +788,24 @@ def sparkline_table(change_df, name, measure):
     filtered = change_df.loc[measure]
 
     # pick entities that start high
-    mask = filtered["is.intlev.initlev"] > filtered["is.intlev.initlev"].quantile(0.8)
+    mask = filtered["is.intlev.initlev"] > filtered[
+        "is.intlev.initlev"
+    ].quantile(0.8)
     filtered = filtered.loc[mask]
 
     # remove entities with a big spike
     mean_std_max = data["rate"].groupby(["code"]).agg(["mean", "std", "max"])
-    mask = mean_std_max["max"] < (mean_std_max["mean"] + (1.96 * mean_std_max["std"]))
+    mask = mean_std_max["max"] < (
+        mean_std_max["mean"] + (1.96 * mean_std_max["std"])
+    )
     filtered = filtered.loc[mask]
 
     # drop duplicates
     filtered = filtered.loc[~filtered.index.duplicated(keep="first")]
 
-    filtered = filtered.sort_values("is.intlev.levdprop", ascending=False).head(10)
+    filtered = filtered.sort_values(
+        "is.intlev.levdprop", ascending=False
+    ).head(10)
     plot_series = sparkline_series(data, "rate", subset=filtered.index)
 
     entity_names = get_entity_names(name, measure)
@@ -779,6 +826,8 @@ def sparkline_table(change_df, name, measure):
 def month_integer_to_dates(input_df, change_df):
     change_df["min_month"] = input_df["month"].min()
     change_df["is.tfirst.big"] = change_df.apply(
-        lambda x: x["min_month"] + pd.DateOffset(months=x["is.tfirst.big"] - 1), axis=1
+        lambda x: x["min_month"]
+        + pd.DateOffset(months=x["is.tfirst.big"] - 1),
+        axis=1,
     )
     return change_df
