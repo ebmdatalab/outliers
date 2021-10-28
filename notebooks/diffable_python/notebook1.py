784c9a05cd09e3d2474cd94d10e9bda15b1576a2
Jon Massey <jon.massey@thedatalab.org>
2021-10-28 09:57:40 +0100

    feat: remove old/duplicate code

--- notebooks/diffable_python/notebook1.py
+++ notebooks/diffable_python/notebook1.py
@@ -14,25 +14,11 @@
 #     name: python3
 # ---
 
-# **This requires > the standard 2GB of memory in docker. It worked with 4GB.**
+from lib.outliers_new import Runner, Report
+from datetime import date
 
-import pandas as pd
-from ebmdatalab import bq
-from lib.outliers import *
+from_date = date(year=2021,month=4,day=1)
+to_date = date(year=2021,month=10,day=1)
+r = Runner(from_date,to_date,5,["practice","ccg","pcn"],False,10)
 
-# ## Load data
-
-# +
-with open("../data/static_outlier_sql/chem_per_para.sql") as sql:
-    query = sql.read()
-chem_per_para = bq.cached_read(query, csv_path='../data/chem_per_para.zip')
-
-## reload specifying data type currently required
-## due to https://github.com/ebmdatalab/datalab-pandas/issues/26
-chem_per_para = pd.read_csv('../data/chem_per_para.zip',dtype={'subpara': str})
-chem_per_para.head()
-# -
-
-# ## Generate HTML for practices, CCGs etc
-
-loop_over_everything(chem_per_para, ['practice','pcn','ccg',])
+r.run()
