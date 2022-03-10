a756eef77a5d24307e9de5df19b7d56cfc34a9ff
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2022-03-10 09:37:44 +0000

    feat: separating results for higher-than-most and lower-than-most

--- notebooks/diffable_python/calculate_summary_statistics.py
+++ notebooks/diffable_python/calculate_summary_statistics.py
@@ -53,12 +53,12 @@ e_counts
 
 # ## Chemical counts
 #
-# Counts of the number of chemicals for which we have data (Z scores etc)
-# within each type of organisation.
+# Counts of the number of unique outlying chemicals (i.e., those identified in the top/bottom
+# 5 z scores) amongst all organisations of the given type.
 
 # +
-### Summarising the number of unique chemicals analysed within
-### each type of organisation
+### Summarising the number of unique chemicals identified in the
+### top/bottom five outliers amongst all organisations of the given type
 
 c_counts = ( e_data.reset_index()[["chemical","entity"]]
             .drop_duplicates()['entity']
@@ -75,44 +75,71 @@ all_counts = e_counts.join( c_counts )
 
 
 # +
-### Calculating summary statistics for the ratio and the Z score
-### within each entity type
-
-all_summary = e_data.groupby( "entity" )[["ratio","z_score"]].describe().reindex(['stp', 'ccg', 'pcn', 'practice']).stack(level=0)
-all_summary = all_summary.rename( columns={"50%":"median"}, inplace=False )
-
-### Defining which metrics will be displayed below
+### Defining which metrics will be displayed in the summary tables
 metrics_to_show = [ "n", "chemicals", "median","max","min","IQR" ]
+
+
+# +
+### Calculating summary statistics for the Z scores for those chemicals
+### identified in the TOP 5 in at least one organisation of the entity type.
+### There are the chemicals displayed in the 'Higher than most' table.
+
+overused_summary = e_data.query('z_score>0').groupby( "entity" )[["z_score"]].describe().reindex(['stp', 'ccg', 'pcn', 'practice']).stack(level=0)
+overused_summary = overused_summary.rename( columns={"50%":"median"}, inplace=False )
+
+# +
+### Calculating summary statistics for the Z scores for those chemicals
+### identified in the BOTTOM 5 in at least one organisation of the entity type.
+### There are the chemicals displayed in the 'Lower than most' table.
+
+underused_summary = e_data.query('z_score<0').groupby( "entity" )[["z_score"]].describe().reindex(['stp', 'ccg', 'pcn', 'practice']).stack(level=0)
+underused_summary = underused_summary.rename( columns={"50%":"median"}, inplace=False )
 # -
 
-
-# ## Summary statistics for the z score in each organisation type
+# ## Summary statistics for outlying Z scores in each organisation type
+#
+# ### Higher than most chemicals
+#
+# The table below summarises the Z scores for the high outlying (i.e., top 5) chemicals
+# in each type of organisation. These are chemicals are seen to be used more often
+# in a particular organisation than its peers.
 
 # +
 ### Extracting the summary statistics for the z scores
-z_tmp = all_summary[all_summary.index.isin(["z_score"], level=1)]
+overused_tmp = overused_summary[overused_summary.index.isin(["z_score"], level=1)]
 
 ### Calculating IQR, removing the row index and rounding to 2dp
-z_summary = ( z_tmp
-         .assign( IQR = z_tmp["75%"]-z_tmp["25%"] )
+overused_toprint = ( overused_tmp
+         .assign( IQR = overused_tmp["75%"]-overused_tmp["25%"] )
          .droplevel(level=1)
          .round(2) )
 
-z_summary.join( all_counts )[metrics_to_show]
+overused_toprint.join( all_counts )[metrics_to_show]
 # -
 
-# ## Summary statistics for the ratio in each organisation type
+# ### Lower than most chemicals
+#
+# The table below summarises the Z scores for the low outlying (i.e., bottom 5) chemicals
+# in each type of organisation. These are chemicals are seen to be used less often
+# in a particular organisation than its peers.
 
 # +
 ### Extracting the summary statistics for the z scores
-ratio_tmp = all_summary[all_summary.index.isin(["ratio"], level=1)]
+underused_tmp = underused_summary[underused_summary.index.isin(["z_score"], level=1)]
 
 ### Calculating IQR, removing the row index and rounding to 2dp
-ratio_summary = ( ratio_tmp
-         .assign( IQR = ratio_tmp["75%"]-ratio_tmp["25%"] )
+underused_toprint = ( underused_tmp
+         .assign( IQR = underused_tmp["75%"]-underused_tmp["25%"] )
          .droplevel(level=1)
          .round(2) )
 
-ratio_summary.join( all_counts )[metrics_to_show]
+underused_toprint.join( all_counts )[metrics_to_show]
 # -
+# ### Summary
+#
+# Below is a summary table that combines the 'Higher than most' and 'Lower than most'
+# results displayed above.
 
+pd.concat([overused_toprint.join( all_counts )[metrics_to_show],
+           underused_toprint[metrics_to_show[2:]]],
+          keys=["Higher than most", "Lower than most"],axis=1)
